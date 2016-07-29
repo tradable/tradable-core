ff6ddf1681698155d7520691b5cde84f76c3e76f
oizulain <oizulain@gmail.com>
2016-07-29 13:06:24 +0200

    .gitignore and gulp task corrections

--- gulpFile.js
+++ gulpFile.js
@@ -20,7 +20,7 @@ gulp.task('test', function() {
       reporter: 'Spec',
       coverage: {
           dir: 'test/coverage',
-          reporters: ['lcovonly', 'text', 'html'],
+          reporters: ['lcovonly'],
           ignore: ['node_modules', 'tests.js', 'dist']
       }
     });
