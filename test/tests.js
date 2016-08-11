8fe829b3e2d2a7ff5f27c1833743cdad072fb23a
oizulain <oizulain@gmail.com>
2016-08-11 15:12:44 +0200

    Corrections

--- test/tests.js
+++ test/tests.js
@@ -1030,7 +1030,7 @@ function getIdentificationToken(type) {
 
     getAnonymousId().then(function(data) {
         var anonId = data.id;
-        var demoAPIAuthenticationRequest = {"appId": tradable.app_id, "type": type, "userIdentification": anonId};
+        var demoAPIAuthenticationRequest = {"appId": tradable.app_id, "type": type, "userIdentification": anonId}; //TODO "appKey": tradable.app_key,
 
         tradable.makeOsRequest("createDemoAccount", "POST", "", "", demoAPIAuthenticationRequest).then(function(token) {
             deferred.resolve(token);
